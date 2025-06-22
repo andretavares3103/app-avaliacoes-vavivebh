791cffa9cf3fc14810ab0beb8188ed45c06f8c1c
andretavares3103 <andre.mtavares1@gmail.com>
2025-06-22 17:21:52 -0300

    Update avaliacoes.py

--- avaliacoes.py
+++ avaliacoes.py
@@ -155,10 +155,11 @@ if not df_atend.empty and "Status Serviço" in df_atend.columns:
 
 
 # -- Coleta do link_id da URL
-query_params = st.experimental_get_query_params()
+query_params = st.query_params
 st.write("DEBUG: query_params = ", query_params)
 link_id = query_params.get("link_id", [None])[0]
 st.write("DEBUG: link_id recebido da query = ", link_id)
+
 
 if link_id:
     dados = buscar_dados(link_id)
